2c1d9a92fa4ee79fb17f21441436618d8b6edeb9
Jasper Jager <mail@jasperjager.com>
2019-01-23 16:49:47 +0100

    Fix relations in states endpoint
    The relations in the state endpoint were broken, because it was still connecting on ‘id’ instead of identification. A small fix was also added for the JSONEncoder. Empty datetimes were being returned as “null” instead of null.

--- src/gobapi/states.py
+++ src/gobapi/states.py
@@ -60,7 +60,7 @@ def _get_valid_states_in_timeslot(timeslot_start, timeslot_end, collection_name,
     if collection_name in relations and valid_states[collection_name]:
         for field, relation in relations[collection_name].items():
             try:
-                relation_entity_id = getattr(valid_states[collection_name], field)['id']
+                relation_entity_id = getattr(valid_states[collection_name], field)['identificatie']
             except KeyError:
                 # If no relation is found, skip checking for other relations
                 pass
@@ -110,7 +110,7 @@ def _calculate_timeslots_for_entity(states, relations, collection_name,  # noqa:
 
             for field, relation in relations[collection_name].items():
                 try:
-                    relation_entity_id = getattr(state, field)['id']
+                    relation_entity_id = getattr(state, field)['identificatie']
                 except KeyError:
                     pass
                 else:
