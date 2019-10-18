ecb9c373c06e0e0e166f8931f54628f3609bb201
vegter <amsjurien@gmail.com>
2019-10-18 10:11:54 +0200

    Add tests for new funtionality

--- src/tests/test_dump.py
+++ src/tests/test_dump.py
@@ -1,9 +1,11 @@
 from unittest import TestCase
-from unittest.mock import MagicMock, patch
+from unittest.mock import MagicMock, patch, ANY
 
 from flask import Response
 
 from gobapi import dump, api, storage
+from gobapi.dump.csv_stream import CSVStream
+from gobapi.dump.to_db import dump_to_db, _dump_to_db
 
 
 class MockEntity:
@@ -88,17 +90,61 @@ class TestConfig(TestCase):
     @patch('gobapi.dump.config.UNIQUE_ID', 'a')
     @patch('gobapi.dump.config.FIELD.SEQNR', 'b')
     def test_get_unique_reference_with_volgnummer(self):
-        result = get_unique_reference(MockEntity(), MockEntity.specs())
+        result = get_unique_reference(MockEntity(), 'a', MockEntity.specs())
         self.assertEqual(result, 'a_5')
 
     @patch('gobapi.dump.config.UNIQUE_ID', 'a')
     @patch('gobapi.dump.config.FIELD.SEQNR', 'c')
     def test_get_unique_reference_without_volgnummer(self):
-        result = get_unique_reference(MockEntity(), MockEntity.specs())
+        result = get_unique_reference(MockEntity(), 'a', MockEntity.specs())
         self.assertEqual(result, 'a')
+
+    @patch('gobapi.dump.config.UNIQUE_ID', 'a')
+    @patch('gobapi.dump.config.REL_UNIQUE_IDS', ['src_a'])
+    @patch('gobapi.dump.config.FIELD.SEQNR', 'c')
+    @patch('gobapi.dump.config.get_field_value', lambda e, f, s: f)
+    def test_get_unique_reference_relation(self):
+        specs = {
+            'src_id': {
+                'type': 'GOB.String',
+                'entity_id': 'a'
+            },
+            'src_c': {
+                'type': 'GOB.Integer'
+            }
+        }
+        result = get_unique_reference(MockEntity(), 'src_a', specs)
+        self.assertEqual(result, 'src_id_src_c')
+
+    @patch('gobapi.dump.config.is_relation', lambda m: True)
+    def test_get_field_specifications_relations(self):
+        model = {
+            'catalog': 'any catalog',
+            'entity_id': 'any entity id name',
+            'all_fields': {
+                'skip_on_type': {'type': 'GOB.VeryManyReference'},
+                '_hash': "skip on field name",
+                'src_id': {'type': 'whatever type'}
+            }
+        }
+        result = get_field_specifications(model)
+        expect = {
+            'src_id': {'type': 'whatever type'},
+            'src_ref': {
+                'description': 'src identificatie_volgnummer or identificatie',
+                'type': 'GOB.String'
+            },
+            'dst_ref': {
+                'description': 'dst identificatie_volgnummer or identificatie',
+                'type': 'GOB.String'
+            }
+        }
+        print(result)
+        self.assertEqual(result, expect)
 
     def test_get_field_specifications(self):
         model = {
+            'catalog': 'any catalog',
             'entity_id': 'any entity id name',
             'all_fields': {
                 'skip_on_type': {'type': 'GOB.VeryManyReference'},
@@ -132,6 +178,7 @@ class TestSQL(TestCase):
         result = dump.sql._create_schema('any_name')
         self.assertTrue("CREATE SCHEMA IF NOT EXISTS \"any_name\"" in result)
 
+    @patch('gobapi.dump.sql.GOBModel', MagicMock())
     @patch('gobapi.dump.sql.get_reference_fields', lambda x: dump.config.REFERENCE_FIELDS)
     @patch('gobapi.dump.sql.get_field_order')
     @patch('gobapi.dump.sql.get_field_specifications')
@@ -139,9 +186,9 @@ class TestSQL(TestCase):
         catalogue = {
             'description': "Any 'catalogue' description"
         }
-        result = dump.sql._create_table(catalogue, 'any_schema', 'any_table', {})
+        result = dump.sql._create_table('any_schema', catalogue, 'any_table', {})
         self.assertTrue("CREATE TABLE IF NOT EXISTS \"any_schema\".\"any_table\"" in result)
-        self.assertTrue("Any ''catalogue'' description" in result)
+        self.assertTrue("COMMENT ON TABLE  \"any_schema\".\"any_table\"" in result)
 
         mock_specs.return_value = {
             'a': {
@@ -178,8 +225,8 @@ class TestSQL(TestCase):
             self.assertTrue(f"character varying" in result)
 
     def test_import_csv(self):
-        result = dump.sql._import_csv('any_schema', 'any_table', 'any_collection')
-        self.assertTrue("\COPY \"any_schema\".\"any_table\" FROM 'any_collection.csv'" in result)
+        result = dump.sql._import_csv('any_schema', 'any_collection', 'any_collection.csv')
+        self.assertTrue("\COPY \"any_schema\".\"any_collection\" FROM 'any_collection.csv'" in result)
 
     @patch('gobapi.dump.sql.GOBModel', MagicMock())
     @patch('gobapi.dump.sql.get_field_order', MagicMock())
@@ -303,6 +350,7 @@ class TestCSV(TestCase):
     def test_csv_entities(self):
         entities = []
         model = {
+            'catalog': 'any catalog',
             'entity_id': 'any entity id',
             'fields': {
                 'any entity id': {
@@ -323,6 +371,7 @@ class TestCSV(TestCase):
 
         entities = [MockEntity(), MockEntity()]
         model = {
+            'catalog': 'any catalog',
             'entity_id': 'a',
             'fields': MockEntity.specs(),
             'all_fields': MockEntity.specs()
@@ -339,6 +388,8 @@ class TestDumpApi(TestCase):
     @patch('gobapi.api.dump_entities', lambda cat, col: ([], {}))
     @patch('gobapi.api.request')
     def test_dump_csv(self, mock_request):
+        mock_request.method = 'GET'
+
         mock_request.args = {'format': 'csv'}
         result = api._dump("any catalog", "any collection")
         self.assertIsInstance(result, Response)
@@ -347,6 +398,8 @@ class TestDumpApi(TestCase):
     @patch('gobapi.api.sql_entities', lambda cat, col, model: [])
     @patch('gobapi.api.request')
     def test_dump_sql(self, mock_request):
+        mock_request.method = 'GET'
+
         mock_request.args = {'format': 'sql'}
         result = api._dump("any catalog", "any collection")
         self.assertIsInstance(result, Response)
@@ -354,6 +407,8 @@ class TestDumpApi(TestCase):
     @patch('gobapi.api.dump_entities', lambda cat, col: ([], {}))
     @patch('gobapi.api.request')
     def test_dump_other(self, mock_request):
+        mock_request.method = 'GET'
+
         mock_request.args = {'format': 'any other format'}
         msg, status = api._dump("any catalog", "any collection")
         self.assertEqual(status, 400)
@@ -362,20 +417,51 @@ class TestDumpApi(TestCase):
         msg, status = api._dump("any catalog", "any collection")
         self.assertEqual(status, 400)
 
+    @patch('gobapi.api.dump_to_db')
+    @patch('gobapi.api.json')
+    @patch('gobapi.api.request')
+    def test_dump_db(self, mock_request, mock_json, mock_dump):
+        mock_request.method = 'POST'
+        mock_request.content_type = 'application/json'
+        mock_json.loads.return_value = {}
+
+        result = api._dump("any catalog", "any collection")
+        mock_dump.assert_called_with("any catalog", "any collection", {})
+
+        mock_request.content_type = 'any content type'
+        mock_dump.reset_mock()
+        result = api._dump("any catalog", "any collection")
+        mock_dump.assert_not_called()
+
 
 class TestDumpStorage(TestCase):
 
     @patch('gobapi.storage._Base', "any base")
     @patch('gobapi.storage.get_session', lambda: MockSession())
-    @patch('gobapi.storage._get_table_and_model', lambda cat, col: ("any table", "any model"))
+    @patch('gobapi.storage._get_table_and_model', lambda cat, col: ("any table", {}))
     def test_dump_entities(self):
         result = storage.dump_entities("any catalog", "any collection")
-        self.assertEqual(result, ('any table', 'any model'))
+        self.assertEqual(result, ('any table', {'catalog': 'any catalog', 'collection': 'any collection'}))
+
+class TestSkipFields(TestCase):
+
+    @patch('gobapi.dump.config.is_relation', lambda m: True)
+    def test_relation_skip_fields(self):
+        model = {
+            'all_fields': {
+                'a': 'any field',
+                **{k: 'any value' for k in dump.config.REL_FIELDS}
+            }
+        }
+        result = dump.config.get_skip_fields(model)
+        self.assertEqual(result, ['a'])
+        self.assertNotEqual(result, [k for k in model['all_fields'].keys()])
 
 class TestFieldOrder(TestCase):
 
     def test_field_order(self):
         model = {
+            'catalog': 'any catalog',
             'entity_id': 'any entity id',
             'fields': {},
             'all_fields': {
@@ -387,6 +473,7 @@ class TestFieldOrder(TestCase):
         self.assertEqual(order, ['any entity id', 'ref'])
 
         model = {
+            'catalog': 'any catalog',
             'entity_id': 'any',
             'fields': {
                 'geo': {'type': 'GOB.Geo.Geometry'},
@@ -404,5 +491,107 @@ class TestFieldOrder(TestCase):
         order = dump.config.get_field_order(model)
         self.assertEqual(order, ['any', 'volgnummer', 'a', 'b', 'ref', 'geo', 'ref', 'meta'])
 
+    @patch('gobapi.dump.config.is_relation', lambda m: True)
+    def test_field_order_relations(self):
+        result = dump.config.get_field_order('any model')
+        self.assertEqual(result, dump.config.REL_FIELDS)
 
+class TestCSVStream(TestCase):
 
+    def test_init(self):
+        stream = CSVStream(iter([]), 1)
+        self.assertIsNotNone(stream)
+
+    def test_empty(self):
+        stream = CSVStream(iter([]), 1)
+        self.assertFalse(stream.has_items())
+
+    def test_only_header(self):
+        stream = CSVStream(iter(["a"]), 1)
+        self.assertTrue(stream.has_items())
+        result = stream.read()
+        self.assertEqual(result, "a")
+
+    def test_one_line(self):
+        stream = CSVStream(iter(["a", "b"]), 1)
+        self.assertTrue(stream.has_items())
+        result = stream.read()
+        self.assertEqual(result, "ab")
+
+    def test_max_read(self):
+        stream = CSVStream(iter(["a", "b", "c"]), 1)
+        self.assertTrue(stream.has_items())
+        result = stream.read()
+        self.assertEqual(result, "ab")
+        stream.reset_count()
+        result = stream.read()
+        self.assertEqual(result, "ac")
+
+    def test_readline(self):
+        stream = CSVStream(iter([]), 1)
+        with self.assertRaises(AssertionError):
+            stream.readline()
+
+class MockStream():
+
+    def __init__(self, *args):
+        self._has_items = True
+
+    def has_items(self):
+        return self._has_items
+
+    def reset_count(self):
+        self._has_items = False
+
+    total_count = 10
+
+class TestToDB(TestCase):
+
+    @patch('gobapi.dump.to_db.create_engine', MagicMock())
+    @patch('gobapi.dump.to_db.URL', MagicMock())
+    @patch('gobapi.dump.to_db.get_relation_name', lambda *args: 'any relation name')
+    @patch('gobapi.dump.to_db._dump_to_db')
+    @patch('gobapi.dump.to_db.dump_entities')
+    def test_dump(self, mock_entities, mock_dump):
+        config = {
+            'db': {},
+        }
+        model = {
+            'references': {
+                'ref': 'any ref'
+            },
+            'very_many_references': {
+                'vmref': 'any very many ref'
+            }
+        }
+        mock_entities.return_value = 'any entities', model
+        mock_dump.return_value = iter([])
+        results = [result for result in dump_to_db('any catalog name', 'any collection name', config)]
+        self.assertEqual(mock_dump.call_count, 3)
+
+        mock_dump.side_effect = lambda *args: 1 / 0
+        results = "".join([result for result in dump_to_db('any catalog name', 'any collection name', config)])
+        self.assertTrue("ERROR: Export failed" in results)
+
+    @patch('gobapi.dump.to_db._create_schema', MagicMock())
+    @patch('gobapi.dump.to_db._create_table', MagicMock())
+    @patch('gobapi.dump.to_db.CSVStream', MockStream)
+    @patch('gobapi.dump.to_db.COMMIT_PER', 1)
+    def test_dump_to_db(self):
+        mock_engine = MagicMock()
+        mock_connection = MagicMock()
+        config = {
+            'engine': mock_engine
+        }
+        model = {
+            'catalog': 'any catalog'
+        }
+        mock_engine.raw_connection.return_value = mock_connection
+        results = [result for result in _dump_to_db("any schema",
+                                                    "any catalog name",
+                                                    "any collection name",
+                                                    iter([]),
+                                                    model,
+                                                    config)]
+        self.assertEqual(mock_connection.commit.call_count, 2)
+        mock_connection.close.assert_called()
