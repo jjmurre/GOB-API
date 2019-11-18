26814fd4367476857a94a4dc3eb36ec28030548f
vegter <amsjurien@gmail.com>
2019-11-18 14:00:22 +0100

    Adjust dump script to retry failing attempts
    The given collection and its relations are dumped one-by-one
    If any dump fails then the dump is retried (max 2 times)

--- src/dump.py
+++ src/dump.py
@@ -27,8 +27,46 @@ for variable in config.keys():
 assert None not in [config[variable] for variable in config.keys()], "Missing environment variables"
 
 
+def get_catalog_collections(dump_api, catalog_name):
+    url = f"{dump_api}/gob/{catalog_name}/"
+    result = requests.get(url=url).json()
+    catalog = {key: value for key, value in result.items() if key not in ['_links', '_embedded']}
+    collections = result['_embedded']['collections']
+    return catalog, collections
+
+
+def get_relations(dump_api, catalog, collection):
+    url = f"{dump_api}/gob/rel/"
+    result = requests.get(url=url).json()
+    abbreviation = f"{catalog['abbreviation']}_{collection['abbreviation']}".lower()
+    relations = result['_embedded']['collections']
+    return [collection for collection in relations if collection['name'].startswith(abbreviation)]
+
+
 def dump_catalog(dump_api, catalog_name, collection_name):
-    url = f"{dump_api}/{catalog_name}/{collection_name}/"
+    catalog, collections = get_catalog_collections(dump_api, catalog_name)
+    if collection_name:
+        collections = [collection for collection in collections if collection['name'] == collection_name]
+
+    schema = catalog_name
+    for collection in collections:
+        dump_collection(dump_api, schema, catalog_name, collection['name'])
+        for relation in get_relations(dump_api, catalog, collection):
+            dump_collection(dump_api, schema, "rel", relation['name'])
+
+
+def dump_collection(dump_api, schema, catalog_name, collection_name):
+    MAX_TRIES = 3
+    tries = 0
+    while tries < MAX_TRIES:
+        tries += 1
+        print(f"Try {tries}: dump {catalog_name} - {collection_name}")
+        if try_dump_collection(dump_api, schema, catalog_name, collection_name):
+            return
+
+
+def try_dump_collection(dump_api, schema, catalog_name, collection_name):
+    url = f"{dump_api}/gob/dump/{catalog_name}/{collection_name}/"
     data = json.dumps({
         "db": {
             "drivername": "postgres",
@@ -36,13 +74,17 @@ def dump_catalog(dump_api, catalog_name, collection_name):
             "password": config['ANALYSE_DATABASE_PASSWORD'],
             "host": config['ANALYSE_DATABASE_HOST_OVERRIDE'],
             "port": config['ANALYSE_DATABASE_PORT_OVERRIDE']
-        }
+        },
+        "schema": schema,
+        "include_relations": False
     })
     headers = {
         "Content-Type": "application/json"
     }
 
+    print(f"Dump {catalog_name} - {collection_name} (schema: {schema})")
     start_request = time.time()
+    success = False
     try:
         result = requests.post(url=url, data=data, headers=headers, stream=True)
 
@@ -56,11 +98,13 @@ def dump_catalog(dump_api, catalog_name, collection_name):
     except Exception as e:
         print(f'ERROR: Export {catalog_name}-{collection_name} failed: {str(e)}')
     else:
-        if not re.match(r'Export completed', lastline):
+        success = re.match(r'Export completed', lastline)
+        if not success:
             print(f'ERROR: Export {catalog_name}-{collection_name} completed with errors')
     finally:
         end_request = time.time()
         print(f"Elapsed time: {(end_request - start_request):.2f} secs")
+    return success
 
 
 if __name__ == '__main__':
@@ -69,7 +113,11 @@ if __name__ == '__main__':
     )
     parser.add_argument('dump_api', type=str, help='e.g. https://acc.api.data.amsterdam.nl/gob/dump')
     parser.add_argument('catalog', type=str, help='e.g. nap')
-    parser.add_argument('collection', type=str, help='e.g. peilmerken')
+    parser.add_argument('collection', type=str, nargs="?", default="", help='e.g. peilmerken')
     args = parser.parse_args()
 
-    dump_catalog(dump_api=args.dump_api, catalog_name=args.catalog, collection_name=args.collection)
+    # Compatibility fix
+    DUMP_API = "/gob/dump"
+    dump_api = args.dump_api.replace(DUMP_API, "")
+
+    dump_catalog(dump_api=dump_api, catalog_name=args.catalog, collection_name=args.collection)
