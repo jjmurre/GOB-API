7a0ce0fc6f0ea87ab3aefee20f1c55ca1bfd0b5d
vegter <amsjurien@gmail.com>
2020-02-27 15:12:26 +0100

    Identify required changes

--- src/gobapi/storage.py
+++ src/gobapi/storage.py
@@ -610,7 +610,7 @@ def get_collection_states(catalog, collection):
     # Get the max sequence number for every id + start validity combination
     sub = session.query(getattr(entity, FIELD.ID),
                         getattr(entity, FIELD.START_VALIDITY),
-                        label("max_seqnr", func.max(cast(getattr(entity, FIELD.SEQNR), Integer)))
+                        label("max_seqnr", func.max(cast(getattr(entity, FIELD.SEQNR), Integer))) NOT NEEDED
                         )\
         .group_by(FIELD.ID, FIELD.START_VALIDITY)\
         .subquery()
@@ -621,7 +621,7 @@ def get_collection_states(catalog, collection):
     all_entities = all_entities \
         .join(sub, and_(getattr(sub.c, FIELD.ID) == getattr(entity, FIELD.ID),
                         getattr(sub.c, FIELD.START_VALIDITY) == getattr(entity, FIELD.START_VALIDITY),
-                        sub.c.max_seqnr == cast(getattr(entity, FIELD.SEQNR), Integer)))\
+                        sub.c.max_seqnr == cast(getattr(entity, FIELD.SEQNR), Integer)))\  NOT NEEDED
         .all()
 
     states = defaultdict(list)
